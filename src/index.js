ac38a75f362aa2520546efc23d67f5b2f6392632
OdetaDz <o.dziugyte@gmail.com>
2023-12-22 10:22:56 +0200

    Available route paths

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import { UsersProvider } from './context/UserContext';
+import { PostsProvider } from "./context/PostContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <UsersProvider>
+        <PostsProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </PostsProvider>
+    </UsersProvider>
 );
